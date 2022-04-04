7882ad8aa38dbe2d5c57df67de574d429578a9ca
sangminw0605 <swoo17@jhu.edu>
2022-04-04 17:00:13 -0400

    Added note functionality

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,13 @@
-function Note () {
-    return <>Note</>;
-  }
-  
-  export default Note;
-  
+import { ListItem, ListItemText } from "@material-ui/core";
+
+function Note(props) {
+  const { note } = props;
+
+  return (
+    <ListItem>
+      <ListItemText primary={note.title} secondary={note.text} />
+    </ListItem>
+  );
+}
+
+export default Note;
