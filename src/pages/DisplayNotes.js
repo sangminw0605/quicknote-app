2efdee401087505148357fb1dd0579394e8fcf0b
sangminw0605 <swoo17@jhu.edu>
2022-04-04 17:24:09 -0400

    Refactored; using DisplayNotes

--- src/pages/DisplayNotes.js
+++ src/pages/DisplayNotes.js
@@ -1,6 +1,29 @@
-function DisplayNotes () {
-    return <>DisplayNotes</>;
+import { List, Fab, withStyles } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
+import Note from "../components/Note";
+
+const styles = {
+  fab: {
+    position: 'absolute',
+    bottom: "2rem",
+    right: "2rem",
   }
-  
-  export default DisplayNotes;
-  
+};
+
+function DisplayNotes(props) {
+  const { notes, deleteNote, classes } = props;
+  return (
+    <>
+      <List>
+        {notes.map((note, index) => {
+          return <Note note={note} key={index} deleteNote={deleteNote} />;
+        })}
+      </List>
+      <Fab aria-label={"Add"} className={classes.fab}>
+        <Add />
+      </Fab>
+    </>
+  );
+}
+
+export default withStyles(styles)(DisplayNotes);
